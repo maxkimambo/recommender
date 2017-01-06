aabeaacfed407551c9c6e36faf1a03a2bf72f8f1
Max Kimambo <max@kimambo.de>
2017-01-06 12:25:25 +0100

    Added threading support

--- main.py
+++ main.py
@@ -1,28 +1,27 @@
 from BackgroundWorker import BackgroundWorker
-
-
+import threading
+import logging
+import time
 def main():
 
-    # #load the app configuration
-    # cfg = ConfigLoader()
-    # config = cfg.get_config()
-    #
-    # print(config)
+    logging.basicConfig(level=logging.DEBUG,
+                        format='[%(levelname)s] (%(threadName)-10s) %(message)s',
+                        )
 
     # server = config.get('server')
-
     # run worker processes if conditions are met
     bgw = BackgroundWorker()
     # generate CB-recommendations
     # implement age check
-    bgw.calculate_content_based_similarity()
+    cb_thread = threading.Thread(name="CB filter thread", target=bgw.calculate_content_based_similarity)
 
     # generate Association Rules
     # implement age check
-    bgw.generate_associative_rules()
+    ar_thread = threading.Thread(name="AR thread", target=bgw.generate_associative_rules)
 
+    cb_thread.start()
+    ar_thread.start()
     # start http server to serve incoming requests
-
 
 if __name__ == "__main__":
     main()
