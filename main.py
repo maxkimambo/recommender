a455b8c32a8f3849661c32365e6ac5ec521449a2
Max Kimambo <max@kimambo.de>
2017-01-10 22:12:05 +0100

    Added command line arguments handling

--- main.py
+++ main.py
@@ -1,27 +1,69 @@
 from BackgroundWorker import BackgroundWorker
+from UserRecommender import UserRecommender
 import threading
-import logging
-import time
-def main():
+from log import Logger
+import sys
+import getopt
 
-    logging.basicConfig(level=logging.DEBUG,
-                        format='[%(levelname)s] (%(threadName)-10s) %(message)s',
-                        )
 
-    # server = config.get('server')
-    # run worker processes if conditions are met
+
+def usage():
+    print("-------------------------------------------------------------------")
+    print('usage..')
+    print('      -u or --update to rebuild the similarity model')
+    print('      -r or --recommend to rebuild the recommendations database')
+    print('      -h or --help to display this message')
+    print("-------------------------------------------------------------------")
+
+def main(sargs):
+
     bgw = BackgroundWorker()
-    # generate CB-recommendations
-    # implement age check
-    #cb_thread = threading.Thread(name="CB filter thread", target=bgw.calculate_content_based_similarity)
+    ur = UserRecommender()
 
-    # generate Association Rules
-    # implement age check
-    ar_thread = threading.Thread(name="AR thread", target=bgw.generate_associative_rules)
+    def start_cb_background_worker():
+        log.debug("Started CB based model generation in the background process")
+        cb_thread = threading.Thread(name="CB filter thread", target=bgw.calculate_content_based_similarity)
+        cb_thread.start()
 
-    #cb_thread.start()
-    ar_thread.start()
-    # start http server to serve incoming requests
+    def start_ar_background_worker():
+        log.debug("Started Association rules mining in the background process.")
+        ar_thread = threading.Thread(name="AR thread", target=bgw.generate_associative_rules)
+        ar_thread.start()
+
+    def update_recommendations_cb():
+        log.debug("Started updating recommendations based on  Collaborative Filtering in the background process.")
+        cb = threading.Thread(name="CB recommender", target=ur.process_cb_recommendations())
+        cb.start()
+
+    def update_recommendations_ar():
+        log.debug("Started updating recommendations based on  Association rules mining in the background process.")
+        ar = threading.Thread(name="AR recommender", target=ur.process_ar_recommendations)
+        ar.start()
+
+    log = Logger()
+
+    ## Parsing command line options
+    long_options = ["update", "ar", "cb", "recommend=", "help"]
+    short_options = "urh"
+    try:
+        opts, arg = getopt.getopt(sargs, short_options, long_options)
+        for opt in opts:
+            if opt[0] in ('-u', '--update'):
+                start_cb_background_worker()
+                start_ar_background_worker()
+            elif opt[0] in ('-r', '--recommend'):
+                update_recommendations_cb()
+                update_recommendations_ar()
+            elif opt[0] in ('-h', '--help'):
+                usage()
+            else:
+                usage()
+    except getopt.GetoptError:
+        print('error')
+        usage()
+        sys.exit(2)
 
 if __name__ == "__main__":
-    main()
+    args = sys.argv[1:]
+    main(args)
+
